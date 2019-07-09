019c850b240da0f8c8c32f1846b90968d7f40b90
Afonso <tomzerabr@hotmail.com>
2019-07-09 20:43:42 -0300

    100%

--- src/main/java/br/edu/utfpr/pb/projetoSeguranca/config/WebSecurityConfig.java
+++ src/main/java/br/edu/utfpr/pb/projetoSeguranca/config/WebSecurityConfig.java
@@ -18,54 +18,55 @@ import br.edu.utfpr.pb.projetoSeguranca.service.impl.UsuarioServiceImpl;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UsuarioServiceImpl usuarioServiceImpl;
+    @Autowired
+    private UsuarioServiceImpl usuarioServiceImpl;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			
-			.exceptionHandling().accessDeniedPage("/403")
-			.and().formLogin().loginPage("/login")
-			.defaultSuccessUrl("/")
-			.failureUrl("/login?error=bad_credentials").permitAll()
-			.and().logout()
-			.logoutSuccessUrl("/login")
-			.and().authorizeRequests()
-				.antMatchers("/genero/**").permitAll()
-				.antMatchers("/produtora/**").permitAll()
-				.antMatchers("/serie/**").permitAll()
-				.antMatchers("/usuario/**").hasAnyRole("ADMIN")
-				.antMatchers("/episodio/**").permitAll()
-				.antMatchers("/**").authenticated();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+
+                .exceptionHandling().accessDeniedPage("/403")
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .failureUrl("/login?error=bad_credentials").permitAll()
+                .and().logout()
+                .logoutSuccessUrl("/login")
+                .and().authorizeRequests()
+                .antMatchers("/genero/**").permitAll()
+                .antMatchers("/produtora/**").permitAll()
+                .antMatchers("/serie/**").permitAll()
+                .antMatchers("/usuario/**").hasAnyRole("ADMIN")
+                .antMatchers("/episodio/**").permitAll()
+                .antMatchers("/**").authenticated();
 //		.hasAnyRole("USER", "ADMIN"
-	}
+    }
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-			.antMatchers("/css/**")
-			.antMatchers("/js/**")
-			.antMatchers("/images/**")
-			.antMatchers("/assets/**")
-			.antMatchers("/webjars/**");
-	}
-	
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return usuarioServiceImpl;
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(10);
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/css/**")
+                .antMatchers("/js/**")
+                .antMatchers("/images/**")
+                .antMatchers("/assets/**")
+                .antMatchers("/vendors/**")
+                .antMatchers("/webjars/**");
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService())
-			.passwordEncoder(passwordEncoder());
-	}
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return usuarioServiceImpl;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
 }
 
 
--- src/main/java/br/edu/utfpr/pb/projetoSeguranca/controller/IndexController.java
+++ src/main/java/br/edu/utfpr/pb/projetoSeguranca/controller/IndexController.java
@@ -7,25 +7,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-	
-	@GetMapping(value = "home")
-	public ModelAndView home() {
-		ModelAndView modelAndView = new ModelAndView("index");
-		
-		return modelAndView;
-	}
-	
-	
-	@GetMapping(value = "")
-	public String index(Model model) {
-		
-		return "redirect:/home";
-	}
 
-	@GetMapping(value = "403")
-	public ModelAndView erro403(Model model) {
-		ModelAndView modelAndView = new ModelAndView("403");
+    @GetMapping(value = "home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("index");
 
-		return modelAndView;
-	}
+        return modelAndView;
+    }
+
+
+    @GetMapping(value = {"", "/"})
+    public String index(Model model) {
+
+        return "redirect:/home";
+    }
+
+    @GetMapping(value = "403")
+    public ModelAndView erro403(Model model) {
+        ModelAndView modelAndView = new ModelAndView("403");
+
+        return modelAndView;
+    }
 }
